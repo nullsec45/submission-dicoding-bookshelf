416e48312c2ac271a9b80d0eca2f2be870d0a002
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-05 23:52:05 +0700

    create and getBook

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,3 +1,7 @@
+import { typeNumber } from "./utils.js";
+import ManageLocalStorage from "./data.js";
+
+
 export default class BookEvent {
     constructor(root) {
         this.root = root;
@@ -5,12 +9,38 @@ export default class BookEvent {
         this.inputBookAuthor = this.root.querySelector("#inputBookAuthor");
         this.inputBookYear = this.root.querySelector("#inputBookYear");
         this.inputPage = this.root.querySelector("#inputPage");
+        this.inputBookIsComplete = this.root.querySelector("#inputBookIsComplete");
 
         this.incompleteBookshelfList = this.root.querySelector("#incompleteBookshelfList");
+        typeNumber(this.inputBookYear);
+        typeNumber(this.inputPage);
+
+        ManageLocalStorage.setLocalStorage;
+        ManageLocalStorage.DATA_BOOKS_LENGTH;
+
+
     }
 
-    saveData() {
+    getBooksList() {
+        return ManageLocalStorage.index;
+    }
 
+    create() {
+        let isComplete = this.inputBookIsComplete.checked == true ? true : false;
+        const title = this.inputBookTitle.value;
+        const author = this.inputBookAuthor.value;
+        const year = this.inputBookYear.value;
+        const page = this.inputPage.value;
+
+
+        ManageLocalStorage.store(
+            title,
+            author,
+            year,
+            isComplete,
+            page
+        )
+        // alert("Buku berhasil ditambahkan");
     }
 
 }
