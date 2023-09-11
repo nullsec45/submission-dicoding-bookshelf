5d75f62d30a0156c5da99112ad1bb312b0d52e96
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-11 23:50:02 +0700

    saveBookToLocalStorage, _loadBookStorage, findBook, isCompleted

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,4 +1,4 @@
-import { STORAGE_KEY, DATA_BOOKS, } from "./constant.js";
+import { STORAGE_KEY, DATA_BOOKS, setDataBooks } from "./constant.js";
 
 
 export default class ManageLocalStorage {
@@ -11,14 +11,37 @@ export default class ManageLocalStorage {
         return true;
     }
 
+    static _saveBookToLocalStorage(book) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(book));
+    }
+
+    static get _loadBooksStorage() {
+        return localStorage.getItem(STORAGE_KEY) ? JSON.parse(localStorage.getItem(STORAGE_KEY)) : [];
+    }
+
     static get index() {
-        const parsed = localStorage.getItem(STORAGE_KEY) ? JSON.parse(localStorage.getItem(STORAGE_KEY)) : [];
-        // console.log(parsed)
-        for (let data of parsed) {
+        const books = this._loadBooksStorage
+
+        for (let data of books) {
             DATA_BOOKS.push(data);
         }
-        console.log(DATA_BOOKS)
         return DATA_BOOKS;
+    }
+
+    static findBook(bookId) {
+        let books = this.index;
+        return books.find((book) => book.id == bookId);
+    }
+
+    static isCompleted(book) {
+        const books = this._loadBooksStorage
+
+        let index = books.map(book => book.title).indexOf(book.title);
+
+        books.splice(index, 1, book);
+
+        this._saveBookToLocalStorage(books);
+        setDataBooks([]);
     }
 
 
@@ -33,7 +56,11 @@ export default class ManageLocalStorage {
             }
         }
         DATA_BOOKS.push({ id, title, author, year, isCompleted });
-        localStorage.setItem(STORAGE_KEY, JSON.stringify(DATA_BOOKS));
+
+        this._saveBookToLocalStorage(DATA_BOOKS);
+
+        setDataBooks([]);
+
         alert("Buku berhasil ditambahkan");
         return true;
     }
